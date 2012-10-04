19cdbe18c4810de18ce4fb3b35430636f6d4eb50
achim <weight.of.fire@googlemail.com>
2012-10-03 21:44:43 -0700

    Added dot metacharacter

--- src/main/java/achim/patternmatcher/PatternMatcher.java
+++ src/main/java/achim/patternmatcher/PatternMatcher.java
@@ -1,23 +1,130 @@
 package achim.patternmatcher;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * Matches patterns against strings. Allows for the following metacharacters in
+ * patterns:
+ * <ul>
+ * <li><code>.</code> - Any char with exactly one occurence.
+ * </ul>
+ * 
  * @author achim, Oct 3, 2012
  */
 public class PatternMatcher {
-	private final String pattern;
+	private final State automaton;
+	private List<State> current = new ArrayList<State>();
 
 	private PatternMatcher(String pattern) {
-		this.pattern = pattern;
+		automaton = analyze(pattern);
 	}
 
 	public boolean matches(String str) {
-		return pattern.equals(str);
+		current.clear();
+		current.add(automaton);
+
+		final List<State> next = new ArrayList<State>();
+		for (char ch : str.toCharArray()) {
+			next.clear();
+
+			for (State s : current)
+				next.addAll(s.nextStatesFor(ch));
+
+			current.clear();
+			current.addAll(next);
+		}
+
+		for (State s : current)
+			if (s.match)
+				return true;
+
+		return false;
 	}
 
+	// Use factory method because we don't wanna throw exceptions from the c'tor
 	public static PatternMatcher create(String pattern) {
 		if (pattern == null)
 			throw new IllegalArgumentException("Pattern must not be null!");
 
 		return new PatternMatcher(pattern);
+	}
+
+	/**
+	 * Builds an automaton from a given matcher-pattern.
+	 * 
+	 * @param pattern The pattern to analyze
+	 * @return The start-state of the automaton
+	 */
+	private State analyze(String pattern) {
+		State s = new State();
+		final State startState = s;
+
+		for (char ch : pattern.toCharArray()) {
+			final Transition t;
+
+			switch (ch) {
+			case '.':
+				t = new MatchAnyCharTransition();
+				break;
+			default:
+				t = new MatchSingleCharTransition(ch);
+				break;
+			}
+
+			s.addTransition(t);
+			t.outcome = new State();
+
+			s = t.outcome;
+		}
+
+		s.match = true;
+
+		return startState;
+	}
+
+	private static class State {
+		private boolean match;
+		private List<Transition> transitions = new ArrayList<Transition>();
+
+		public void addTransition(Transition t) {
+			transitions.add(t);
+		}
+
+		public List<State> nextStatesFor(char ch) {
+			List<State> reachable = new ArrayList<State>(transitions.size());
+
+			for (Transition t : transitions)
+				if (t.mayTransist(ch))
+					reachable.add(t.outcome);
+
+			return reachable;
+		}
+	}
+
+	private static abstract class Transition {
+		private State outcome;
+
+		public abstract boolean mayTransist(char ch);
+	}
+
+	private static class MatchAnyCharTransition extends Transition {
+		@Override
+		public boolean mayTransist(char ch) {
+			return true;
+		}
+	}
+
+	private static class MatchSingleCharTransition extends Transition {
+		private final char matchChar;
+
+		public MatchSingleCharTransition(char matchChar) {
+			this.matchChar = matchChar;
+		}
+
+		@Override
+		public boolean mayTransist(char ch) {
+			return matchChar == ch;
+		}
 	}
 }
--- src/test/java/achim/patternmatcher/PatternMatcherTest.java
+++ src/test/java/achim/patternmatcher/PatternMatcherTest.java
@@ -21,6 +21,30 @@ public class PatternMatcherTest {
 		createMatcher(null);
 	}
 
+	@Test
+	public void match_shouldUseDotAsPlaceholderForOneChar_WithRandomChar() {
+		PatternMatcher m = createMatcher("t.st");
+		assertTrue(m.matches("tast"));
+	}
+
+	@Test
+	public void match_shouldUseDotAsPlaceholderForOneChar_WithNoCharGivenShouldNotMatch() {
+		PatternMatcher m = createMatcher("t.st");
+		assertFalse(m.matches("tst"));
+	}
+
+	@Test
+	public void match_shouldUseDotAsPlaceholderForOneChar_WithRandomCharButTooShort() {
+		PatternMatcher m = createMatcher("t.st");
+		assertFalse(m.matches("tas"));
+	}
+
+	@Test
+	public void match_shouldUseDotAsPlaceholderForOneChar_WithRandomCharButTooLong() {
+		PatternMatcher m = createMatcher("t.st");
+		assertFalse(m.matches("taste"));
+	}
+
 	private PatternMatcher createMatcher(String pattern) {
 		return PatternMatcher.create(pattern);
 	}
